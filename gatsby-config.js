a347703afc35868cf031e05b99cf5b0b8d02d963
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-07-14 14:08:31 +0530

    create sourcenodes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sass`
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,53 @@
 import React from "react"
+import { graphql } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>
+import '../styles.scss'
+
+export const query = graphql`
+{
+  allRestaurant {
+    totalCount
+    nodes {
+      restaurant {
+        name
+        average_cost_for_two
+        featured_image
+        location {
+          latitude
+          longitude
+        }
+        url
+        user_rating {
+          aggregate_rating
+        }
+        id
+        photos_url
+      }
+    }
+  }
+}
+`
+
+
+export default ({ data }) => {
+  return (<main>
+    <h2>List of top restaurants in zomato</h2>
+    <div className='container'>
+      <div className="cards-container">
+        {data.allRestaurant.nodes.map(({ restaurant }) => {
+          const { name, id, average_cost_for_two, featured_image, photos_url, url, user_rating } = restaurant
+          return (<div key={id} className='card'>
+            <h3>{name}</h3>
+            <p>{average_cost_for_two}</p>
+            <img src={featured_image || photos_url} alt={name} />
+            <p>{user_rating.aggregate_rating}</p>
+            <a href={url}>Go to restaurant page</a>
+            {/*   <p>{`${location.latitude}, ${location.longitude}`}</p> */}
+          </div>)
+        }
+        )}
+      </div>
+    </div>
+
+  </main>)
 }
