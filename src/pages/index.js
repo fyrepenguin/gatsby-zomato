1b102a076b95173fc77d851eeb87e4ba072c578f
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-07-15 02:03:02 +0530

    add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 import { graphql } from 'gatsby';
-import { Divider, Card, Typography, Header } from "antd";
+import { Divider, Card, Typography, Layout } from "antd";
 import 'antd/dist/antd.css'
 
 import palceholderImage from '../assets/placeholder.jpg'
 import '../styles.scss'
 
 const { Title, Paragraph, Text } = Typography
-
-
+const { Header, Content, Footer } = Layout
 
 export const query = graphql`
 {
@@ -37,37 +36,35 @@ export const query = graphql`
 
 
 const IndexPage = ({ data }) => {
-  return (<main>
-    <Header>List of top restaurants in zomato</Header>
-    <div className='container'>
-      <div className="cards-container">
-        {data.allRestaurant.nodes.map(({ restaurant }) => {
-          const { name, id, average_cost_for_two, featured_image, url, user_rating, location } = restaurant
-          return (
+  return (
+    <Layout>
+      <Header className='header' style={{ height: 78 }}><Title>List of top restaurants in Chennai</Title></Header>
+      <Content style={{ paddingTop: '3rem' }}>
+        <div className='container'>
+          <div className="cards-container">
+            {data.allRestaurant.nodes.map(({ restaurant }) => {
+              const { name, id, average_cost_for_two, featured_image, url, user_rating, location } = restaurant
+              return (
 
-            <Card hoverable={true} style={{ width: 240 }} key={id} cover={<img src={featured_image || palceholderImage} alt={name} />}>
-              <h3>{name}</h3>
-              <Paragraph><Text strong>Average Price: </Text>₹{average_cost_for_two}</Paragraph>
-              <Paragraph><Text strong>User Rating: </Text>{user_rating.aggregate_rating}/5</Paragraph>
-              <div className='links'>
+                <Card hoverable key={id} cover={<img src={featured_image || palceholderImage} alt={name} />}>
+                  <Title level={3}>{name}</Title>
+                  <Paragraph><Text strong>Average Price: </Text>₹{average_cost_for_two}</Paragraph>
+                  <Paragraph><Text strong>User Rating: </Text>{user_rating.aggregate_rating}/5</Paragraph>
+                  <div className='links'>
 
-                <a href={url} target='_blank' rel="noreferrer">View More</a>
-                <Divider type='vertical' style={{ height: 'auto', borderColor: "rgba(0,0,0,0.65)" }} />
-                <a href={`https://maps.google.com/?q=${location.latitude},${location.longitude}`} target='_blank' rel="noreferrer">View Location</a>
-              </div>
-            </Card>
-          )
+                    <a href={url} target='_blank' rel="noreferrer">View More</a>
+                    <Divider type='vertical' style={{ height: 'auto', borderColor: "rgba(0,0,0,0.65)" }} />
+                    <a href={`https://maps.google.com/?q=${location.latitude},${location.longitude}`} target='_blank' rel="noreferrer">View Location</a>
+                  </div>
+                </Card>
+              )
 
-        }
-        )}
-      </div>
-    </div>
-
-  </main>)
-}
-
-export default IndexPage
-{/* <div key={id} className='card'>
+            }
+            )}
+          </div>
+        </div>
+      </Content>
+      {/* <div key={id} className='card'>
             <div className='img-container'>
               <img src={featured_image || palceholderImage} alt={name} />
             </div>
@@ -82,3 +79,8 @@ export default IndexPage
             </div>
 
           </div> */}
+      <Footer>Developed By <a href="https://www.github.com/fyrepenguin" target='_blank' rel="noreferrer">Mourya</a></Footer>
+    </Layout>)
+}
+
+export default IndexPage
